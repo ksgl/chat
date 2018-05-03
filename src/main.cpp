1c1fc6c05165085ed2525bef0b648ff8196fce09
ksgl <ksenia.glazacheva@gmail.com>
2018-05-03 13:57:44 +0300

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,7 @@
 #include "Udp_server.hpp"
 #include <boost/asio.hpp>
 
-//3 параметра : порт, который слушаем, ip/hostname кому слать, dest-port
-
-//p2p nat
-
 int main() {
-	/*try {
-		boost::asio::io_service io_service;
-		Udp_server server(io_service);
-		io_service.run();
-	}
-	catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}*/
-
-	boost::asio::io_service io_service;
-	Udp_server server(io_service, "8.8.8.8", "1337");
-
-	/*std::string msg;
-	server.receive(msg);
-	std::cout << "Your message: " << msg;
-	
-	io_service.run();
-	*/
-
-	server.send("Hi to Google from Ksusha");
-
     return EXIT_SUCCESS;
 }
 
