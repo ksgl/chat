dda3ff59e4392070a3c65402b0543d8b84952633
Petr <www.petrovich08@mail.ru>
2018-05-07 03:31:04 +0300

    Initial_commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,9 @@
 #include <iostream>
 #include "task_manager.hpp"
 
-void tasker_1(task_manager &obj) {
-    json j;
-    j["task"] = "some fucking task";
-    
-    for(int i = 0; i < 10; i++)
-        obj.add_task_on_queue(j);
-}
-
-void tasker_2(task_manager &obj) {
-    json j;
-    j["task"] = "another fucking task";
-    
-    for(int i = 0; i < 10; i++)
-        obj.add_task_on_queue(j);
-}
-
-task_manager &task_manager_obj = task_manager::instance();
+TaskManager &task_manager_obj = TaskManager::instance();
 
 int main() {
-    thread task_1(tasker_1, task_manager_obj);
-    thread task_2(tasker_2, task_manager_obj);
-    
-    task_1.join();
-    task_2.join();
-    
-    task_manager_obj.show_tasks();
     
     return EXIT_SUCCESS;
 }
--- src/task_manager.hpp
+++ src/task_manager.hpp
@@ -2,39 +2,34 @@
 
 #include <iostream>
 #include <queue>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 #include <mutex>
 
 using json = nlohmann::json;
 
-class task_manager {
+class TaskManager {
 public:
-	static task_manager &instance();
-	void add_task_to_queue(json obj);
-    void show_tasks() {
-        task_queue_mutex.lock();
-        std::cout << task_queue.pop() << endl;
-        ask_queue_mutex.unlock();
-    }
+	static TaskManager &instance();
+	void add_task_to_queue(json task);
 private:
-	task_manager() = default;
-	task_manager(const task_manager &obj) = delete;
-	task_manager &operator=(const task_manager &obj) = delete;
+	TaskManager() = default;
+	TaskManager(const TaskManager &obj) = delete;
+	TaskManager &operator=(const TaskManager &obj) = delete;
 
 	std::queue<json> task_queue;
 	std::mutex task_queue_mutex;
 };
 
-task_manager &task_manager::instance() {
-	static task_manager single_obj;
+TaskManager &TaskManager::instance() {
+	static TaskManager single_obj;
 
 	return single_obj;
 }
 
-void task_manager::add_task_to_queue(json obj) {
+void TaskManager::add_task_to_queue(json task) {
 	task_queue_mutex.lock();
 
-	task_queue.push_back(obj);
+	task_queue.push(task);
 
 	task_queue_mutex.unlock();
 }
